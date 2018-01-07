2e136550f72b3c30a467fbb7315c48a21bbd2d5a
rjgagui <robert.gaguitr@gmail.com>
2018-01-07 20:05:19 +0800

    added new Logo component then apply it to Toolbar component

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@
 import React, { Component } from 'react';
 
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
 
 class Toolbar extends Component {
     render() {
         return (
             <header className={classes.Toolbar}>
                 <div>Menu</div>
-                <div>Logo</div>
+                <Logo />
                 <nav>....</nav>
             </header>
         );
