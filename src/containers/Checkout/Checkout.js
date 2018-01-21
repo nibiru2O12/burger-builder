edbb560fd8fded6e7a3559859fa9ca63a648ef74
rjgagui <robert.gaguitr@gmail.com>
2018-01-21 16:18:07 +0800

    move data submission on server to Checkout Containers

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-import ContactData from './ContactData/ContactData';
+import ContactData from '../ContactData/ContactData';
 import Spinner from '../../UI/Spinner/Spinner';
+import axiosOrder from '../../axios-instances/axios-order';
 
 class Checkout extends Component{
 
     state = {
-        ingredients:{}
+        ingredients:{},
+        isLoading:false
     }
 
     componentDidMount(){
@@ -23,7 +25,7 @@ class Checkout extends Component{
             return true;
         }
 
-        this.props.history.replace('/');
+        //this.props.history.replace('/');
 
     }
 
@@ -35,8 +37,36 @@ class Checkout extends Component{
         this.props.history.replace(`${this.props.location.pathname}/contact-details`);
     }
 
+
+    handleSubmitOrder = (e,customer) =>{
+
+        this.setState({isLoading:true});
+        
+        const {ingredients} = this.state;
+
+        axiosOrder.post('/orders.json',{
+            customer,ingredients
+        })
+        .then(response => {
+            this.setState({isLoading:false});
+            console.log("data" , this.state )
+        })
+        .catch(err => this.setState({isLoading:false}))
+
+        alert('Thanks for ordering');
+
+        this.props.history.replace('/');
+        //prevent submiting form
+        e.preventDefault();
+     }
+
+
     render(){
         const {match} = this.props;
+
+        if(this.state.isLoading){
+            return <Spinner />
+        }
 
         return(
             <div>
@@ -46,7 +76,9 @@ class Checkout extends Component{
                     onContinue = {this.handleContinueCheckout} />
 
                 <Route path={`${match.url}/contact-details`} 
-                        render={()=>(<ContactData ingredients={this.state.ingredients} />)}/>
+                        render={()=>(
+                            <ContactData submitOrder={this.handleSubmitOrder} />
+                        )}/>
             </div>
         );
     }
---
+++ src/containers/ContactData/ContactData.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
+
+import Button from '../../UI/Button/Button';
+import classes from './ContactData.css';
+import Spinner from '../../UI/Spinner/Spinner';
+
+class ContactData extends Component {
+    state = { 
+        customer: {
+            name:'',
+            address:{
+                country:'',
+                zip:'',
+                street:''
+            },
+            email:"",
+            deliveryMethod:"COD"
+        } 
+    }
+
+     componentDidMount(){
+        this.setState({ingredients:this.props.ingredients});
+     }
+
+     handleContactChange = (e) => {
+        const {target} = e;
+        let customer = {...this.state.customer};
+
+        customer[target.name]=target.value;
+
+        this.setState({
+            customer:customer
+        });
+     }
+
+     handleAddressChange = (e) =>{
+        const {target} = e;
+        let address = {...this.state.customer.address};
+
+        address[target.name]=target.value;
+
+        this.setState({
+            address:address
+        });
+     }
+
+    render() {
+
+        return (
+            <div className={classes.ContactData}>
+                
+                <h4>Enter tour Contact Data</h4>
+                <form>
+                    <input type="text" 
+                            name="name" 
+                            placeholder='Enter yout name'
+                            onChange={e => this.handleContactChange(e)} 
+                            value={this.state.customer.name} />
+                    <input type="email" 
+                            name="email" 
+                            placeholder="something@email.com" 
+                            onChange={e => this.handleContactChange(e)}
+                            value={this.state.customer.email} />
+                    <input type="text" 
+                            name="street"
+                            value={this.state.customer.address.street} />
+                    <input type="text" 
+                            name="zip"
+                            value={this.state.customer.address.zip} />
+                    <Button btnType="Success" 
+                            onClick={(e) => {
+                                this.props.submitOrder(e,this.state.customer);
+                                e.preventDefault();
+                            }}>
+                            ORDER
+                    </Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default withRouter(ContactData);
